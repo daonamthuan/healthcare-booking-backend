a80adf529436e6ff28d98b6a71a60808fe39128a
DaoNamThuan <21522649@gm.uit.edu.vn>
2024-04-17 19:56:56 +0700

    change some feature on server

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,6 @@
 import userService from "../services/userService";
 
 let handleLogin = async (req, res) => {
-    // api phai tra ve status
     let email = req.body.email;
     let password = req.body.password;
 
@@ -15,9 +14,29 @@ let handleLogin = async (req, res) => {
 
     let userData = await userService.handleUserLogin(email, password);
 
-    return res.status(200).json(userData);
+    // api phai tra ve status
+    return res.status(200).json(userData); // chi tra json
+};
+
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id; // ALL or ID
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: "Missing required parameters",
+            users: [],
+        });
+    }
+    let users = await userService.getAllUsers(id);
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: "OK",
+        users,
+    });
 };
 
 module.exports = {
     handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
 };
--- src/server.js
+++ src/server.js
@@ -3,17 +3,20 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
 import connectDB from "./config/connectDB";
+import cors from "cors";
 
 import dotenv from "dotenv";
-dotenv.config();  // goi ham config cua thu vien dotenv
+dotenv.config(); // goi ham config cua thu vien dotenv
 
 let app = express();
+app.use(cors({ credentials: true, origin: true }));
+// app.use(cors({ origin: true }));
 
 //config app, cau hinh express co the doc va xu ly cac yeu cau duoi dang json va form
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-viewEngine(app); 
+viewEngine(app);
 initWebRoutes(app);
 
 connectDB();
@@ -22,4 +25,4 @@ let port = process.env.PORT || 6969; // if port is undefined => port = 6969
 
 app.listen(port, () => {
     console.log("Backend Nodejs is running on the port : " + port);
-})
+});
